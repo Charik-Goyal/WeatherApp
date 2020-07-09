2fce8e50b69802a016c872d52d1c5c72fac02db9
Charik goyal <charikgoyal8@gmail.com>
2020-07-09 19:13:40 +0530

    The changes in about page and in API information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lat,long,callback) => {
             callback('unable to reach the location',undefined)
         }
         else{
-            callback(undefined,'It is currently ' + body.weather[0].description  +' Temperature is (in Celsius) ' + body.main.temp +' degree ' + ' the chances of getting rain is ' + body.clouds.all + ' %'
+            callback(undefined,'It is currently ' + body.weather[0].description  +' Temperature is (in Celsius) ' + body.main.temp +' degree ' + 'And it feels like ' + body.main.feels_like + ' degree ' + ' and Humidity in atmosphere is ' + body.main.humidity + '% ' + ' the chances of getting rain is ' + body.clouds.all + ' %' + ' Max Temp today ' + body.main.temp_max + ' degree ' + 'and Min temp today is ' + body.main.temp_min + ' degree '
             )
         }
     })
